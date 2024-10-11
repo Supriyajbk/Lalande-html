c0997c2fdb0524429c62392e3a20355b4ffbf582
sumeethajbk <sumeetha@jbkinfotech.com>
2024-10-11 15:24:00 +0530

    commot

--- js/custom-slick.js
+++ js/custom-slick.js
@@ -90,4 +90,47 @@ jQuery(document).ready(function () {
     })
   }
 
+  jQuery('.slider-for').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: false, // Ensure fade is enabled
+    dots: false,
+    asNavFor: '.slider-nav',
+    infinite: true,
+    draggable: true,
+    swipeToSlide: true,
+    touchThreshold: 100,
+    cssEase: 'ease-in-out',
+    speed: 1500,
+});
+
+var wcslider = jQuery('.slider-nav');
+wcslider.slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    arrows: true,
+    infinite: true,
+    focusOnSelect: true,
+    draggable: true,
+    swipeToSlide: true,
+    touchThreshold: 100,
+    appendArrows: jQuery('.custom_arrows_wc'),
+    prevArrow: '<div class="custom_arrow custom_arrow_dir_left"></div>',
+    nextArrow: '<div class="custom_arrow custom_arrow_dir_right"></div>',
+    appendDots: jQuery('.custom_dots_wc'),
+    customPaging: function (wcslider, i) {
+      var thumb = jQuery(wcslider.$slides[i]).data();
+      return '0' + (i + 1); // Add numbering for dots
+    },
+    dotsClass: 'custom_dots_list',
+});
+
+    
+  
+
+
+
 });
