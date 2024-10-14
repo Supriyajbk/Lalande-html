3186c92ceee3bfd67d7b75a1996296b5b60371c7
sumeethajbk <sumeetha@jbkinfotech.com>
2024-10-14 14:38:31 +0530

    commot

---
+++ stage/404.php
@@ -0,0 +1,17 @@
+<?php
+get_header();
+?>
+<section class="page-not-section">
+    <div class="container">
+        <div class="page-not-main aligncenter">
+            <div class="page-not-text">
+                <h1>404</h1>
+                <h2>Oops !</h2>
+                <p>The page you are looking for might have been removed had its name changed or is temporarily
+                    unavailable.</p>
+                    <a href="<?php echo site_url(); ?>" class="button back">Back to Home</a>
+            </div>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
